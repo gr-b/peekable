e7de1186575fed91b7f265fbe2e888b982ce25fc
Gagan cm <cm.g@northeastern.edu>
2026-03-08 17:21:00 -0400

    Update UI flow, layout, and window behavior

--- src/main/preload.js
+++ src/main/preload.js
@@ -7,6 +7,8 @@ contextBridge.exposeInMainWorld('peekable', {
   saveOnboarding: (config) => ipcRenderer.invoke('save-onboarding', config),
   saveSettings: (config) => ipcRenderer.invoke('save-settings', config),
   getConfig: () => ipcRenderer.invoke('get-config'),
+  startMonitoring: () => ipcRenderer.invoke('start-monitoring'),
+  hideWindow: () => ipcRenderer.invoke('hide-window'),
   restartApp: () => ipcRenderer.invoke('restart-app'),
   onShowPasswordPrompt: (callback) => ipcRenderer.on('show-password-prompt', callback),
   __testCompleteOnboarding: (data) => ipcRenderer.invoke('test-complete-onboarding', data)
